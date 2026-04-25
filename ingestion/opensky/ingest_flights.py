a644b079f472405f9f0e55cf072cf646e0b91600
Vasyl Tsykolanov <vtsykolanov@chinaski.local>
2026-04-25 14:55:21 +0200

    Refactor ingestion logic in ingest_flights.py to improve error handling and data processing flow

--- ingestion/opensky/ingest_flights.py
+++ ingestion/opensky/ingest_flights.py
@@ -18,40 +18,50 @@ default_config = {
     "volume": 'opensky'
 }
 
-spark.sql(f"""
-    CREATE EXTERNAL VOLUME IF NOT EXISTS
-    {default_config['unity_catalog']}.{default_config['schema']}.{default_config['volume']}
-    LOCATION 'abfss://{default_config["schema"]}@{default_config["storage"]}.dfs.core.windows.net/{default_config["volume"]}/'
-""")
+try:
 
-# Get current date parts
-now = datetime.now(ZoneInfo("Europe/Paris"))
-year  = now.strftime("%Y")
-month = now.strftime("%m")
-day = now.strftime("%d")
+    logger.info("Ingesting opensky files into raw volume...")
 
-volume_path = f"/Volumes/{default_config['unity_catalog']}/{default_config['schema']}/{default_config['volume']}"
-file_path = f"{volume_path}/{year}/{month}/flights_day_{day}.json"
+    spark.sql(f"""
+        CREATE EXTERNAL VOLUME IF NOT EXISTS
+        {default_config['unity_catalog']}.{default_config['schema']}.{default_config['volume']}
+        LOCATION 'abfss://{default_config["schema"]}@{default_config["storage"]}.dfs.core.windows.net/{default_config["volume"]}/'
+    """)
 
-COLUMNS = [
-    "icao24", "callsign", "origin_country", "time_position",
-    "last_contact", "longitude", "latitude", "baro_altitude",
-    "on_ground", "velocity", "true_track", "vertical_rate",
-    "sensors", "geo_altitude", "squawk", "spi", "position_source"
-]
+    # Get current date parts
+    now = datetime.now(ZoneInfo("Europe/Paris"))
+    year  = now.strftime("%Y")
+    month = now.strftime("%m")
+    day = now.strftime("%d")
 
-opensky_url = "https://opensky-network.org/api/states/all"
-PARAMS = {
-    # "lamin": 49.0, "lomin": -11.3, "lamax": 61.0, "lomax": 2.0,  # UK example
-}
-response = requests.get(opensky_url, params = PARAMS, timeout=30)
-response.raise_for_status()
-payload = response.json()
+    volume_path = f"/Volumes/{default_config['unity_catalog']}/{default_config['schema']}/{default_config['volume']}"
+    file_path = f"{volume_path}/{year}/{month}/flights_day_{day}.json"
 
-records = [
-    {**dict(zip(COLUMNS, state)), "api_timestamp": now.isoformat()}
-    for state in payload.get("states", [])
-    if state is not None
-]
+    COLUMNS = [
+        "icao24", "callsign", "origin_country", "time_position",
+        "last_contact", "longitude", "latitude", "baro_altitude",
+        "on_ground", "velocity", "true_track", "vertical_rate",
+        "sensors", "geo_altitude", "squawk", "spi", "position_source"
+    ]
 
-dbutils.fs.put(file_path, json.dumps(records, indent=2, default=str), overwrite=True)
+    opensky_url = "https://opensky-network.org/api/states/all"
+    PARAMS = {
+        # "lamin": 49.0, "lomin": -11.3, "lamax": 61.0, "lomax": 2.0,  # UK example
+    }
+    response = requests.get(opensky_url, params = PARAMS, timeout=30)
+    response.raise_for_status()
+    payload = response.json()
+
+    records = [
+        {**dict(zip(COLUMNS, state)), "api_timestamp": now.isoformat()}
+        for state in payload.get("states", [])
+        if state is not None
+    ]
+
+    dbutils.fs.put(file_path, json.dumps(records, indent=2, default=str), overwrite=True)
+
+except Exception as e:
+    logger.error("Error ingesting OpenSky data: %s", str(e))
+    raise
+
+logger.info("Successfully ingested OpenSky data. Pipeline completed.")
